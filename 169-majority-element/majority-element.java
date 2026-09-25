c0872d3f3f079c5187509760dc64e2c86d92608e
THANNEERU-RAMCHARAN-TEJA <ugs24341_cse.thanneeru@cbit.org.in>
2026-09-25 18:27:40 +0530

    Time: 1 ms (99.88%) | Memory: 63.4 MB (6.38%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,20 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap <Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int val=nums[i];
-            hm.put(val,hm.getOrDefault(val,0)+1);
-        }
-        int ans = 0;
-        int n=nums.length/2;
-        for(int macha:hm.keySet()){
-            int value=hm.get(macha);
-            if(value>n){
-            ans=macha;
-            break;
+        int count = 0;
+        int el = 0;
+        for(int val:nums){
+            if(count ==0){
+                count++;
+                el = val;
+            }
+            else if(el == val){
+                count++;
+            }
+            else{
+                count--;
             }
         }
-        return ans;
+        return el;
 
     }
 }
